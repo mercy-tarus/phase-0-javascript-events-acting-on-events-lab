27be26dda23d4798def4ec7abbba3557cc914066
mercy-tarus <merciechelah1@gmail.com>
2024-04-19 12:10:16 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+/** @format */
+
 // Your code here
+
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  let left = dodger.style.left.replace("px", "");
+  let leftPosition = parseInt(left, 10);
+
+  if (leftPosition > 0) {
+    dodger.style.left = `${leftPosition - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let left = dodger.style.left.replace("px", "");
+  let leftPosition = parseInt(left, 10);
+
+  if (leftPosition < 360) {
+    dodger.style.left = `${leftPosition + 1}px`;
+  }
+}
